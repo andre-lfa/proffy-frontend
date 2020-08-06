b3b06f5426c0f7127c20c35ca8e7eb30a9389bee
Andre LFA <andrelfaareas@gmail.com>
2020-08-06 12:21:48 -0300

    Creates the card with the teachers information

--- src/pages/Landing/TeacherList/index.tsx
+++ src/pages/Landing/TeacherList/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../../components/PageHeader';
 
+import whatsappIcon from '../../../assets/images/icons/whatsapp.svg';
+
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
@@ -24,6 +26,34 @@ function TeacherList() {
                     </div>
                 </form> 
             </PageHeader>
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars0.githubusercontent.com/u/33098462?s=460&u=a51b9ea995f188368a62f333a8f43ca0aca360a7&v=4" alt=""/>
+                        <div>
+                            <strong>André LFA</strong>
+                            <span>Quimica</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Entusiasta das melhores tecnologias de química avançada.
+                        <br/><br/>
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida de pessoas através de experiências.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }

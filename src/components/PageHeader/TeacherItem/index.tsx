11d6d6914c1f803213a3b808c2a59f0f1e51648e
Andre LFA <andrelfaareas@gmail.com>
2020-08-09 16:32:57 -0300

    Connects the teacher listing page to the API

--- src/components/PageHeader/TeacherItem/index.tsx
+++ src/components/PageHeader/TeacherItem/index.tsx
@@ -3,34 +3,46 @@ import whatsappIcon from '../../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars0.githubusercontent.com/u/33098462?s=460&u=a51b9ea995f188368a62f333a8f43ca0aca360a7&v=4" alt=""/>
-                        <div>
-                            <strong>André LFA</strong>
-                            <span>Quimica</span>
-                        </div>
-                    </header>
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida de pessoas através de experiências.
-                    </p>
+            <p>
+                {teacher.bio}
+            </p>
 
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong>R$ 80,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
+            <footer>
+                <p>
+                    Preço/hora
+                            <strong>{teacher.cost}</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
                             Entrar em contato
                         </button>
-                    </footer>
-                </article>
+            </footer>
+        </article>
     )
 }
 
